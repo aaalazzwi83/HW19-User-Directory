85a40e1c566b35f85aaec338f5c997b105e2c169
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-04-20 22:49:48 -0700

    Updates

---
+++ precache-manifest.5f64fcb10c68c1b588cafeb256dd818a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befe96e0aa3d5c19489de3b2dccd1f95",
+    "url": "/HW19-User-Directory/index.html"
+  },
+  {
+    "revision": "ab646951ef0cdd02ceff",
+    "url": "/HW19-User-Directory/static/css/main.ab6266af.chunk.css"
+  },
+  {
+    "revision": "2e415b2aee3d4cbbc1c2",
+    "url": "/HW19-User-Directory/static/js/2.791aaf9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW19-User-Directory/static/js/2.791aaf9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab646951ef0cdd02ceff",
+    "url": "/HW19-User-Directory/static/js/main.d71af2d6.chunk.js"
+  },
+  {
+    "revision": "45646ce1dbc6455b5d50",
+    "url": "/HW19-User-Directory/static/js/runtime-main.9dd529a5.js"
+  }
+]);
